bb17111c57d045ee5bd122cbe9a49c4f109959c4
Gui Seek <guiseek@gmail.com>
2023-08-31 11:10:31 -0300

    refactor: state

--- src/core/state.ts
+++ src/core/state.ts
@@ -3,17 +3,17 @@ type GetType<T extends Value> = T extends number ? number : T;
 type SetFn<T extends Value> = (value: GetType<T>) => void;
 type GetFn<T extends Value> = () => GetType<T>;
 
-export const useState = <T extends Value>(value?: T) => {
-  const prop = new Text(value?.toLocaleString() ?? "");
+export const useState = <T extends Value>(initialValue?: T) => {
+  const element = new Text(initialValue?.toLocaleString() ?? "");
 
-  const setValue = (value: T) => {
-    prop.nodeValue = value.toLocaleString();
+  const update = (state: T) => {
+    element.nodeValue = state.toLocaleString();
   };
 
-  const getValue = () => {
-    const value = prop.nodeValue ?? "";
-    return !isNaN(+value) ? +value : (value as GetType<T>);
+  const value = () => {
+    const state = element.nodeValue ?? "";
+    return !isNaN(+state) ? +state : (state as GetType<T>);
   };
 
-  return [setValue, prop, getValue] as [SetFn<T>, Text, GetFn<T>];
+  return [update, element, value] as [SetFn<T>, Text, GetFn<T>];
 };
